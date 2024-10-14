2bfdc813ec920ec6135f3ecda450a4f050674ca0
Karina <kbrilhante@gmail.com>
2024-10-14 20:19:13 -0300

    Update wheelSegment.js

--- wheelSegment.js
+++ wheelSegment.js
@@ -15,6 +15,29 @@ class WheelSegment {
         stroke(0);
         fill(this.color);
         arc(this.x, this.y, this.radius, this.radius, this.startAngle, this.endAngle, PIE);
+        
+        push()
+        stroke(255)
+        const angle = this.startAngle + this.sliceSize / 2;
+        const vector = p5.Vector.fromAngle(angle);
+        vector.mult(this.radius * 0.6);
+        vector.add(this.x, this.y);
+        strokeWeight(2)
+        stroke(0)
+        fill(255);
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        let txt = "";
+        if (this.type == "points") {
+            txt = this.value;
+        } else {
+            txt = this.type.replaceAll("_", " ").toUpperCase()
+        }
+        translate(vector.x, vector.y);
+        rotate(angle);
+        text(txt, 0, 0)
+
+        pop()
     }
     spin(speed) {
         this.startAngle += speed;
