b4bee0e457683946340c90347625509c835df3c4
Karina <kbrilhante@gmail.com>
2024-10-14 19:22:35 -0300

    Update wheelSegment.js

--- wheelSegment.js
+++ wheelSegment.js
@@ -1,5 +1,5 @@
 class WheelSegment {
-    constructor(x, y, radius, color, startAngle, sliceSize) {
+    constructor(x, y, radius, color, startAngle, sliceSize, type, value = 0) {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -7,11 +7,17 @@ class WheelSegment {
         this.sliceSize = sliceSize;
         this.startAngle = startAngle;
         this.endAngle = startAngle + sliceSize;
+        this.type = type;
+        this.value = value;
     }
     show() {
         strokeWeight(2);
         stroke(0);
         fill(this.color);
         arc(this.x, this.y, this.radius, this.radius, this.startAngle, this.endAngle, PIE);
+
+        
+
+
     }
 }
